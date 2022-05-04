f28f7a8618f04ec41a2b90e117cfc39cc63d2ba8
oscarsp5 <perezsaldana.oscar@gmail.com>
2022-05-04 10:20:07 +0200

    Se actualiza el proyecto

--- Codigo/proyecto.sh
+++ Codigo/proyecto.sh
@@ -6,7 +6,83 @@
 
 function fpermisos(){
 	
-rutafichero=$(yad --width=400 --height=350 --title "Selecciona el fichero" --form --center --field=fichero:FL --column="") 2> /dev/null
+#Seleccion archivo
+
+archivo=$(yad --file \
+		--title="Proyecto-bash-Oscar" \
+		--height=200 \
+		--width=100 \
+		--center \
+		--text="Selecciona un archivo:" \
+		--file-filter="scripts | *.sh")
+ans=$?
+		
+if [ $ans -eq 0 ]
+	then
+		echo "Has elegido este archivo: ${archivo}"
+	else
+		echo "No has elegido ningún archivo"
+fi
+	
+#Seleccion permisos
+	
+#Permisos Propietario
+	
+ppropietario=$(yad --list \
+			--title="Proyecto-bash-Oscar" \
+			--height=70 \
+			--width=35 \
+			--center \
+			--text="Establece permisos a propietario:" \
+			--entry --button=Establecer:0 )
+					
+while [ $ppropietario -gt 7 ]
+	do
+		yad --width=400 --height=50 --center --text-align=center --column="" --text="Escriba un número entre el 0 y 7"
+		ppropietario=$(yad --width=400 --height=50 --title "Permisos para propietarios" --center --text-align=center \
+		--text="Se ha pasado de 7 vuelva a escribir el numero:" --entry --button=Establecer:0 )		
+	done
+		
+#Permisos Grupos
+		
+pgrupo=$(yad --list \
+      --title="Proyecto-bash-Oscar" \
+      --height=70 \
+      --width=35 \
+      --center \
+      --text="Establece permisos a grupos:" \
+      --entry --button=Establecer:0 )
+	
+while [ $pgrupo -gt 7 ]
+	do
+		yad --width=400 --height=50 --center --text-align=center --column="" --text="Escriba un número entre el 0 y 7"
+		pgrupo=$(yad --width=400 --height=50 --title "Permisos para grupos" --center --text-align=center \
+		--text="Se ha pasado de 7 vuelva a escribir el numero:" --entry --button=Establecer:0 )
+	done
+	
+potros=$(yad --list \
+      --title="Proyecto-bash-Oscar" \
+      --height=70 \
+      --width=35 \
+      --center \
+      --text="Establece permisos a otros" \
+      --entry --button=Establecer:0 )
+	
+while [ $potros -gt 7 ]
+	do
+		yad --width=400 --height=50 --center --text-align=center --column="" --text="Escriba un número entre el 0 y 7"
+		potros=$(yad --width=400 --height=50 --title "Permisos para otros" --center --text-align=center \
+		--text="Se ha pasado de 7 vuelva a escribir el numero:" --entry --button=Establecer:0 )
+	done
+  
+  
+permisos="$ppropietario$pgrupo$potros"
+	chmod $permisos $archivo
+	comls=$(ls -l $archivo)
+	echo $comls
+	
+	resultado=$(yad --width=400 --height=300 --title "Estos son los permisos que has seleccionado" --center --text="${comls}")
+
 
 }
 
